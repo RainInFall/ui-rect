1903da829c42e6243207697d1f001e9b9dcf54e0
rongqiansong <lygjcsrq@163.com>
2016-12-26 12:42:37 +0800

    fix ui-point dependcy

---
+++ index.js
@@ -0,0 +1,129 @@
+var Point = require('ui-point');
+
+function Rect(leftTopPoint, rightBottomPoint) {
+  if (!(this instanceof Rect)) {
+    return new Rect();
+  }
+  this._ltPoint = leftTopPoint.clone();
+  this._lbPoint = Point(leftTopPoint.x(), rightBottomPoint.y());
+  this._rtPoint = Point(rightBottomPoint.x(), leftTopPoint.y());
+  this._rbPoint = rightBottomPoint.clone();
+}
+
+Rect.isRect = function(r) {
+  return r instanceof Rect;
+};
+
+Rect.validate = function(leftTopPoint, rightBottomPoint) {
+  return leftTopPoint.x() <= rightBottomPoint.x() && leftTopPoint.y() <= rightBottomPoint.y();
+};
+
+Rect.prototype.validate = function() {
+  return Rect.validate(this.leftTop(), this.rightBottom());
+};
+
+Rect.union = function(r1, r2) {
+  var left = Math.max(r1.left(), r2.left());
+  var right = Math.min(r1.right(), r2.right());
+  var top = Math.max(r1.top(), r2.top());
+  var bottom = Math.min(r1.bottom(), r2.bottom());
+
+  var ltPoint = Point(left, top);
+  var rbPoint = Point(right, bottom);
+
+  if ( !Rect.validate(ltPoint, rbPoint) ) {
+    return null;
+  }
+
+  return new Rect(ltPoint, rbPoint);
+};
+
+Rect.prototype.union = function(another) {
+  return Rect.union(this, another);
+};
+
+Rect.split = function(rect, x, y) {
+  var rects = [];
+  for (var i = 0; i < x; i++) {
+    var column = [];
+    for (var j = 0; j < y; j++) {
+      var lt = Point(rect.x() * i / x, rect.y() * j / y);
+      var rb = Point(rect.x() * (i+1) / x, rect.y() * (j+1) / y);
+      column.push(new Rect(lt, rb));
+    }
+    rects.push(column);
+  }
+  return rects;
+};
+
+Rect.prototype.split = function(x, y) {
+  return Rect.split(this, x, y);
+};
+
+Rect.prototype.center = function() {
+  return Point((this._ltPoint.x() + this._rbPoint.x())/2, (this._ltPoint.y() + this._rbPoint.y())/2);
+};
+
+Rect.prototype.left = function() {
+  return this._ltPoint.x();
+};
+
+Rect.prototype.right = function() {
+  return this._rbPoint.x();
+};
+
+Rect.prototype.top = function() {
+  return this._ltPoint.y();
+};
+
+Rect.prototype.bottom = function() {
+  return this._rbPoint.y();
+};
+
+Rect.prototype.leftTop = function() {
+  return this._ltPoint.clone();
+};
+
+Rect.prototype.leftBottom = function() {
+  return this._lbPoint.clone();
+};
+
+Rect.prototype.rightTop = function() {
+  return this._rtPoint.clone();
+};
+
+Rect.prototype.rightBottom = function() {
+  return this._rbPoint.clone();
+};
+
+Rect.prototype.centerTop = function() {
+  return Point(this.center().x(), this.top());
+};
+
+Rect.prototype.centerBottom = function() {
+  return Point(this.center().x(), this.bottom());
+};
+
+Rect.prototype.leftCenter = function() {
+  return Point(this.left(), this.center().y());
+};
+
+Rect.prototype.rightCenter = function() {
+  return Point(this.right(), this.center().x());
+};
+
+Rect.prototype.width = function() {
+  return this.right() - this.left();
+};
+
+Rect.prototype.height = function() {
+  return this.bottom() - this.top();
+};
+
+Rect.prototype.aera = function() {
+  return this.width() * this.height();
+};
+
+Rect.Point = Point;
+
+module.exports = Rect;
--- test/test.js
+++ test/test.js
@@ -1,3 +1,25 @@
-var uiRect = require("ui-rect");
+var Rect = require("..");
 var expect = require('expect');
- 
+var Point = Rect.Point;
+
+describe('Rect', function(){
+  it('should work', function(){
+    var rect = new Rect(Point(0,0), Point(3, 4));
+    expect(Rect.isRect(rect)).toBe(true);
+  });
+});
+
+describe('Rect#left Rect#right Rect#top Rect#bottom', function(){
+  it('should correct', function(){
+    var rect = new Rect(Point(0,0), Point(3, 4));
+    expect(rect.left()).toBe(0);
+    expect(rect.top()).toBe(0);
+    expect(rect.right()).toBe(3);
+    expect(rect.bottom()).toBe(4);
+  });
+  it('should not effect by init value', function(){
+    var lt = Point(3, 4);
+    var rb = Point(4, 5);
+    
+  });
+});
